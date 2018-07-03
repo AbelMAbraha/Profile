ff89bb5c2127b7c8799809e38cf4ea7e2bff0213
AbelMAbraha <AbelMAbraha@hotmail.com>
2018-07-04 01:03:35 +0200

    .

--- Profile/Scripts/Modal.js
+++ Profile/Scripts/Modal.js
@@ -1,4 +1,19 @@
-﻿var modal = document.getElementById('myModal');
+﻿$(window).scroll(function callOnceOnScroll() {
+    var $window = $(window),
+        $document = $(document);
+    if ($window.scrollTop() >= $document.height() - $window.height() - 300 &&
+        $window.scrollTop() <= $document.height() - $window.height() - 200) {
+        $("#skillsInfo > div:nth-child(2) > div > div > div").css({ "transition": "1s", "height": "10px", "width": "90%" });
+        $("#skillsInfo > div:nth-child(3) > div > div > div").css({ "transition": "1s", "height": "10px", "width": "90%" });
+        $("#skillsInfo > div:nth-child(4) > div > div > div").css({ "transition": "1s", "height": "10px", "width": "70%" });
+        $("#skillsInfo > div:nth-child(5) > div > div > div").css({ "transition": "1s", "height": "10px", "width": "40%" });
+        $("#skillsInfo > div:nth-child(6) > div > div > div").css({ "transition": "1s", "height": "10px", "width": "40%" });
+        $("#skillsInfo > div:nth-child(7) > div > div > div").css({ "transition": "1s", "height": "10px", "width": "50%" });
+    }
+});
+
+
+var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("adjPfSt");
